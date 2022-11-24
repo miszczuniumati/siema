33381e98211e2f7511c464099b8badc8c3b262ef
Mateusz Kriese <mateusz.kriese7@gmail.com>
2022-11-24 11:36:06 +0100

    24.11.22

---
+++ 22.11.22/gropc.js
@@ -0,0 +1,3 @@
+function sendImg(){
+const img = document.getElementById('img')
+}
---
+++ 24.11.22/zadegz1.js
@@ -0,0 +1,12 @@
+function calculate(sign){
+    const first=document.getElementById('firstNumber').value;
+    const second=document.getElementById('secondNumber').value;
+    const result=document.getElementById('result');
+
+    if(first.length==0 || second.length==0){
+        console.log('')
+    }
+    else{
+        console.log('jest dobrze')
+    }
+}
